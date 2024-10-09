6bf05f8697093e4477775b0316b2a06ac7a06aa6
antopineda <antonia.pineda@uc.cl>
2024-10-09 15:29:01 -0300

    add consultas

--- cargador/parametros_tablas.php
+++ cargador/parametros_tablas.php
@@ -13,7 +13,9 @@ $path_tablas = array(
 $tablas_iniciales = array(
     'asignaturas' => 'plan VARCHAR(10), asignatura_id VARCHAR(6) PRIMARY KEY, asignatura VARCHAR(50), nivel INT',
     'notas' => 'RUN INT NOT NULL, num_alumno VARCHAR(10), periodo VARCHAR(7), codigo VARCHAR(10), convocatoria VARCHAR(3), calificacion VARCHAR(2), nota DECIMAL(4,2)',
-    'estudiantes' => 'codigo_plan VARCHAR(3), carrera VARCHAR(20), cohorte VARCHAR(7), bloqueo CHAR, causal_bloqueo VARCHAR(50), RUN VARCHAR(10) PRIMARY KEY, nombre VARCHAR(50), apellido VARCHAR(50), email VARCHAR(50), plan VARCHAR(10), logro VARCHAR(50), fecha_logro VARCHAR(7), ultima_carga VARCHAR(7)',
+    'estudiantes' => 'codigo_plan VARCHAR(3), carrera VARCHAR(20), cohorte VARCHAR(7), bloqueo CHAR, causal_bloqueo VARCHAR(50), RUN VARCHAR(10) PRIMARY KEY, nombre VARCHAR(50), apellido VARCHAR(50), plan VARCHAR(10), logro VARCHAR(50), fecha_logro VARCHAR(7), ultima_carga VARCHAR(7)',
+    'historial_académico' => 'RUN INT NOT NULL, num_alumno VARCHAR(10), periodo VARCHAR(7), codigo VARCHAR(10), convocatoria VARCHAR(3), calificacion VARCHAR(2), nota DECIMAL(4,2)',
+    
 );
 
 ?>
